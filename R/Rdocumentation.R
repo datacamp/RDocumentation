933a6dc7e23fe58e3d95980e33ccea5bf3ec1c7a
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-07-19 18:47:03 +0200

    working, including reviews

--- R/Rdocumentation.R
+++ R/Rdocumentation.R
@@ -1,5 +1,6 @@
 rdocs_url = function(){
-  return("http://rdocs-staging.herokuapp.com/")
+  return("http://localhost:1337/link/")
+  #return("http://rdocsv2-app-staging.us-west-1.elasticbeanstalk.com/")
 }
 
 help_path <- function(function_name){ 
@@ -10,21 +11,41 @@ get_help = function(function_name){
   browseURL(help_path(function_name))
 }
 
-help <- function( all_fields=NULL, package_name=NULL, function_name=NULL,
-                  title=NULL, description=NULL, author=NULL ){
+help <- function(function_name,package_name=NULL){
   args = as.list(environment());
   if( all( sapply(args,FUN="is.null"))){
     stop("Please provide a function, package,... to search for")
   } 
 
-  go_to_url = paste0(rdocs_url(),"advanced_search?utf8=✓", 
-            "&q=",as.character(all_fields), 
-            "&package_name=", as.character(package_name), 
-            "&function_name=", as.character(function_name),
-            "&title=", as.character(title), 
-            "&description=", as.character(description), 
-            "&author=", as.character(author) )
-  browseURL(go_to_url)
+  #go_to_url = paste0(rdocs_url(),"advanced_search?utf8=✓", 
+            #"&q=",as.character(all_fields), 
+            #"&package_name=", as.character(package_name), 
+            #"&function_name=", as.character(function_name),
+            #"&title=", as.character(title), 
+            #"&description=", as.character(description), 
+            #"&author=", as.character(author) )
+  if(!is.null(function_name)){
+    if(!is.null(package_name)){
+      go_to_url=paste0(rdocs_url(),as.character(function_name),"/",as.character(package_name),"/ordered")
+    }
+    else{
+      go_to_url=paste0(rdocs_url(),as.character(function_name),"/ordered")
+    }
+    viewer <- getOption("viewer")
+    if (!is.null(viewer)){
+      tempDir <- tempfile()
+      dir.create(tempDir)
+      htmlFile <- file.path(tempDir, "index.html")
+      download.file(go_to_url, destfile = htmlFile,method='internal',quiet=TRUE)
+      viewer(htmlFile)
+    }
+    else{
+      utils::browseURL(go_to_url)
+    }
+  }
+  else{
+    print("please input a valid function name and or package name")
+  }  
 }
 
 `?` <- function (e1, e2) 
