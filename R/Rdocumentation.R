fe9cccb5939449c721a959e92e8bfb03eec6dc49
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-07-20 18:29:23 +0200

    working version

--- R/Rdocumentation.R
+++ R/Rdocumentation.R
@@ -3,27 +3,11 @@ rdocs_url = function(){
   #return("http://rdocsv2-app-staging.us-west-1.elasticbeanstalk.com/")
 }
 
-help_path <- function(function_name){ 
-  paste0(rdocs_url(),"#",function_name)  
-}
-
-get_help = function(function_name){
-  browseURL(help_path(function_name))
-}
-
 help <- function(function_name,package_name=NULL){
   args = as.list(environment());
   if( all( sapply(args,FUN="is.null"))){
     stop("Please provide a function, package,... to search for")
   } 
-
-  #go_to_url = paste0(rdocs_url(),"advanced_search?utf8=✓", 
-            #"&q=",as.character(all_fields), 
-            #"&package_name=", as.character(package_name), 
-            #"&function_name=", as.character(function_name),
-            #"&title=", as.character(title), 
-            #"&description=", as.character(description), 
-            #"&author=", as.character(author) )
   if(!is.null(function_name)){
     if(!is.null(package_name)){
       go_to_url=paste0(rdocs_url(),as.character(function_name),"/",as.character(package_name),"/ordered")
@@ -48,28 +32,7 @@ help <- function(function_name,package_name=NULL){
   }  
 }
 
-`?` <- function (e1, e2) 
+`?` <- function (e1, e2=NULL) 
 {
-  
-  if (missing(e2)) {
-    type <- NULL
-    topicExpr <- substitute(e1)
-  }
-  else {
-    type <- substitute(e1)
-    topicExpr <- substitute(e2)
-  }
-  
-  # Capture the "??" situation and do a global search 
-  search <- (is.call(topicExpr) && topicExpr[[1L]] == "?")
-  
-  if(search){
-    return( help( topicExpr[[2L]] ) )
-  }
-  
-  if(substr(as.character(topicExpr),2,2)[[1]]=="?"){
-    topicExpr <- substr(as.character(topicExpr),3,nchar(topicExpr)) 
-  }
-  
-  get_help(as.character(topicExpr))
+  help(e1,e2)
 }
