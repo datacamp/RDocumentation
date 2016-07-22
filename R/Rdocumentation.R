6307a189e84b87d4f236ea03b5d134b6ece2bbf2
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-07-22 16:33:25 +0200

    package and function documentation working

--- R/Rdocumentation.R
+++ R/Rdocumentation.R
@@ -2,131 +2,90 @@ rdocs_url = function(){
   return("http://localhost:1337/link/")
   #return("http://rdocsv2-app-staging.us-west-1.elasticbeanstalk.com/")
 }
+rdocs_package_url=function(){
+  return("http://localhost:1337/")
+}
 
-help <- function(function_name,package_name=NULL){
-  args = as.list(environment());
-  if( all( sapply(args,FUN="is.null"))){
-    stop("Please provide a function, package,... to search for")
-  } 
-  if(!is.null(function_name)){
-    if(!is.null(package_name)){
-      go_to_url=paste0(rdocs_url(),as.character(function_name),"/",as.character(package_name),"/ordered")
+help <- function(topic=NULL,package=NULL,try.all.packages = getOption("help.try.all.packages")){
+  ischar <- tryCatch(is.character(topic) && length(topic) == 
+        1L, error = identity)
+  if (inherits(ischar, "error")) 
+      ischar <- FALSE
+  if (!ischar) {
+      reserved <- c("TRUE", "FALSE", "NULL", "Inf", "NaN", 
+          "NA", "NA_integer_", "NA_real_", "NA_complex_", "NA_character_")
+      stopic <- deparse(substitute(topic))
+      if (!is.name(substitute(topic)) && !stopic %in% reserved) 
+          stop("'topic' should be a name")
+      if(!stopic %in% reserved){
+        topic <- stopic
+      }      
+  }
+  ischar <- tryCatch(is.character(package) && length(topic) == 
+        1L, error = identity)
+    if (inherits(ischar, "error")) 
+        ischar <- FALSE
+    if (!ischar) {
+      reserved <- c("TRUE", "FALSE", "NULL", "Inf", "NaN", 
+          "NA", "NA_integer_", "NA_real_", "NA_complex_", "NA_character_")
+        spackage <- deparse(substitute(package))
+        if (!is.name(substitute(package))) 
+          stop("'package' should be a name")
+        if(!spackage %in% reserved){
+          package<-spackage   
+        }          
     }
-    else{
-      go_to_url=paste0(rdocs_url(),as.character(function_name),"/ordered")
-    }
-    viewer <- getOption("viewer")
-    if (!is.null(viewer)){
-      tempDir <- tempfile()
-      dir.create(tempDir)
-      htmlFile <- file.path(tempDir, "index.html")
-      download.file(go_to_url, destfile = htmlFile,method='internal',quiet=TRUE)
-      viewer(htmlFile)
-    }
-    else{
-      utils::browseURL(go_to_url)
+  if ((!is.null(topic)) && is.null(package) &&  try.all.packages==FALSE) {
+      for (p in search()){
+        if(topic %in% ls(pos=p)){
+            package = as.character(p)
+        
+      }
     }
   }
+  else if(is.null(package) && try.all.packages){
+    packages=c()
+    for (lib in .libPaths()) {
+            packages <- c(packages,.packages(TRUE, lib))
+        }
+  }
+  else if(!is.null(package)){
+     
+  }
+  if(!is.null(topic)){
+    if(!is.null(package)){
+      go_to_url=paste0(rdocs_url(),as.character(topic),"/",as.character(package),"/ordered")
+    }
+    else if(try.all.packages){
+      array =""
+      for(p in packages){
+        array = paste0(array,p,",")
+      }
+      array = substr(array,1,nchar(array)-1)  
+      go_to_url=paste0(rdocs_url(),as.character(topic),"/",array,"/ordered")
+    }
+    else{
+      go_to_url=paste0(rdocs_url(),as.character(topic),"/",as.character(package),"/ordered")
+    }
+  }
+  else if (!is.null(package)){
+    go_to_url=paste0(rdocs_url(),"NULL/",as.character(package),"/ordered")
+  }
+  viewer <- getOption("viewer")
+  if (!is.null(viewer)){
+    tempDir <- tempfile()
+    dir.create(tempDir)
+    htmlFile <- file.path(tempDir, "index.html")
+    download.file(go_to_url, destfile = htmlFile,method='internal',quiet=TRUE)
+    viewer(htmlFile)
+  }
   else{
-    print("please input a valid function name and or package name")
-  }  
+    utils::browseURL(go_to_url)
+  }
+
 }
 
 `?` <- function (e1, e2=NULL) 
 {
   help(e1,e2)
 }
-# =======
-  
-#   if (missing(e2)) {
-#     type <- NULL
-#     topicExpr <- substitute(e1)
-#   }
-#   else {
-#     type <- substitute(e1)
-#     topicExpr <- substitute(e2)
-#   }
-  
-#   # Capture the "??" situation and do a global search 
-#   search <- (is.call(topicExpr) && topicExpr[[1L]] == "?")
-  
-#   if(search){
-#     return( help( topicExpr[[2L]] ) )
-#   }
-  
-#   if(substr(as.character(topicExpr),2,2)[[1]] == "?"){
-#     topicExpr <- substr(as.character(topicExpr),3,nchar(topicExpr)) 
-#   }
-#   get_help(as.character(topicExpr))
-# }
-
-# `?` <- function (e1, e2) 
-# {
-#   if (missing(e2)) {
-#     type <- NULL
-#     topicExpr <- substitute(e1)
-#   }
-#   else {
-#     type <- substitute(e1)
-#     topicExpr <- substitute(e2)
-#   }
-#   search <- (is.call(topicExpr) && topicExpr[[1L]] == "?")
-  
-#   if(search){
-#     return( help(topicExpr[[2L]]) )
-#   }
-  
-#   if(substr(as.character(topicExpr),2,2)[[1]] == "?"){
-#     topicExpr <- substr(as.character(topicExpr),3,nchar(topicExpr)) 
-#     get_help(as.character(topicExpr))
-#   }
-
-#   if (is.call(topicExpr) && (topicExpr[[1L]] == "::" || topicExpr[[1L]] == 
-#                              ":::")) {
-#     package <- as.character(topicExpr[[2L]])
-#     topicExpr <- topicExpr[[3L]]
-#   }
-#   else package <- NULL
-#   if (search) {
-#     if (is.null(type))
-#       return(eval(substitute(help.search(TOPIC, package = PACKAGE),
-#                              list(TOPIC = as.character(topicExpr), PACKAGE = package))))
-#     else return(eval(substitute(help.search(TOPIC, fields = FIELD,
-#                                             package = PACKAGE), list(TOPIC = as.character(topicExpr),
-#                                                                      FIELD = as.character(type), PACKAGE = package))))
-#   }
-#   else {
-#     if (is.null(type)) {
-#       if (is.call(topicExpr))
-#         return(.helpForCall(topicExpr, parent.frame()))
-#       topic <- if (is.name(topicExpr))
-#         as.character(topicExpr)
-#       else e1
-#       return(eval(substitute(help(TOPIC, package = PACKAGE),
-#                              list(TOPIC = topic, PACKAGE = package))))
-#     }
-#     else {
-#       type <- if (is.name(type))
-#         as.character(type)
-#       else e1
-#       topic <- if (is.name(topicExpr))
-#         as.character(topicExpr)
-#       else {
-#         if (is.call(topicExpr) && identical(type, "method"))
-#           return(.helpForCall(topicExpr, parent.frame(),
-#                               FALSE))
-#         e2
-#       }
-#       if (type == "package")
-#         package <- topic
-#       h <- .tryHelp(topicName(type, topic), package = package)
-#       if (is.null(h)) {
-#         if (is.language(topicExpr))
-#           topicExpr <- deparse(topicExpr)
-#         stop(gettextf("no documentation of type %s and topic %s (or error in processing help)",
-#                       sQuote(type), sQuote(topicExpr)), domain = NA)
-#       }
-#       h
-#     }
-#   }
-# }
