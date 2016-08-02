bc1ecd2d9b95b377323f7e31c0335a5e606fd942
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-08-02 10:30:53 +0200

    Run examples works

--- R/Rdocumentation.R
+++ R/Rdocumentation.R
@@ -2,18 +2,41 @@
 rdocs_url <- function(){
   return("http://localhost:1337/")
 }
+.onLoad<-function(libName,packageName){
+    login()
+}
+#' @export
+login<-function(){
+    library(rjson)
+    library(httr)  
+    #the jsonlite package turns every variable into an array, which the /login doesn't accept, so the json is parsed with rjson
+    if(file.exists(paste0(.libPaths()[1],"/Rdocumentation/config/creds.txt")) && file.info(paste0(.libPaths()[1],"/Rdocumentation/config/creds.txt"))$size>0){
+        creds<-read.table(paste0(.libPaths()[1],"/Rdocumentation/config/creds.txt"),header=FALSE)
+        go_to_url=paste0(rdocs_url(),"login")
 
-.pkgglobalenv <- new.env(parent=emptyenv())
+        tryCatch({
+                r <- POST(go_to_url,body=as.character(creds$V1),content_type("application/x-www-form-urlencoded"))
+                if(status_code(r)!=200){
+                    print("there is something wrong with your credentials, please try logging in to the syte in the help panel")
+                }
+                else{
+                    print("logging you in to Rdocumentation")
+                }
+            },
+            error=function(cond){
+                print(cond)
+                print("Could not log you in, something is wrong with your internet connection or Rdocumentation is offline")
+            }
+        )
+    }
+    else{
+         dir.create(paste0(.libPaths()[1],"/Rdocumentation/config"),showWarnings = FALSE,recursive=TRUE)
+    }
+}
 
 #overwrites the class<- function, converts help answers to json and sends them to Rdocumentation
 `.class.help<-`<-function(package,value){
-    if(exists('s_id',envir=.pkgglobalenv)==FALSE){
-        session_id=""
-    }
-    else{
-        session_id=.pkgglobalenv$s_id
-    }
-    library(jsonlite)
+    library(rjson)
     if(value== "help_files_with_topic"){
         packages<-lapply(package,function(path){
             temp = strsplit(path,"/")[[1]]
@@ -25,9 +48,8 @@ rdocs_url <- function(){
         })
         body= toJSON(list(packages=as.character(paste(packages,sep="",collapse=",")),topic_names=as.character(paste(topic_names,sep="",collapse=",")),
         call=as.character(paste(attributes(package)$call,sep="",collapse=",")),topic=as.character(attributes(package)$topic),
-        tried_all_packages=as.character(attributes(package)$tried_all_packages),help_type=as.character(attributes(package)$type),
-        XS_Secret=as.character(Sys.getenv("RS_SHARED_SECRET")), Rstudio_port= as.character(Sys.getenv("RSTUDIO_SESSION_PORT"))))
-        go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/normal/help")
+        tried_all_packages=as.character(attributes(package)$tried_all_packages),help_type=as.character(attributes(package)$type)))
+        go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/normal/help?viewer_pane=1")
     }
     else{
         hsearch_db_fields <-c("alias", "concept", "keyword", "name", "title")
@@ -39,53 +61,61 @@ rdocs_url <- function(){
         type=as.character(package[3]),agrep=as.character(package[4]),ignore_case=as.character(package[5]),
         types=as.character(paste(package$types,sep="",collapse=",")),package=as.character(package[7]),
         matching_titles=as.character(gsub(" ", "", toString(unique(package$matches$Topic)), fixed = TRUE)),
-        matching_packages=as.character(gsub(" ", "", toString(unique(package$matches$Package)), fixed = TRUE)),
-        XS_Secret=as.character(Sys.getenv("RS_SHARED_SECRET")), Rstudio_port= as.character(Sys.getenv("RSTUDIO_SESSION_PORT"))))
-        go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/search/help")
+        matching_packages=as.character(gsub(" ", "", toString(unique(package$matches$Package)), fixed = TRUE))))
+        go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/search/help?viewer_pane=1")
     }
-    return (.view_help(go_to_url,body,session_id,TRUE))
+    return (.view_help(go_to_url,body,TRUE))
 }
 
 #TODO make a package url that doesn't return JSON
 .browseUrl.help<-function(url,browser){
-     if(exists('s_id',envir=.pkgglobalenv)==FALSE){
-        session_id=""
-    }
-    else{
-        session_id=.pkgglobalenv$s_id
-    }
     parsing = substring(url,18,nchar(url)-18)
     parts = strsplit(parsing,"/")
-    go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/package/",parts[[1]][3])
-    return (.view_help(go_to_url,NULL,session_id,FALSE))
+    go_to_url=paste0(Rdocumentation::rdocs_url(),"rstudio/package/",parts[[1]][3],"?viewer_pane=1")
+    return (.view_help(go_to_url,NULL,FALSE))
 }
-.view_help<-function(go_to_url,body,session_id,post){
-    print(session_id)
+.view_help<-function(go_to_url,body,post){
     tempDir <- paste0(.libPaths()[1],"/Rdocumentation/doc")
     htmlFile <- file.path(tempDir, "index.html")
-    dir.create(tempDir)
-    if(post){
-        r <- POST(go_to_url,set_cookies("sails.sid=" = as.character(session_id)),config=(content_type_json()),add_headers(Cookie= paste0("sails.sid=",as.character(session_id))),body =body,encode="json")
-        print(content(r))
-        print(content(r)$cookies)
-        writeBin(content(r,'raw'),htmlFile)
+    if(!file.exists(tempDir)){
+        dir.create(tempDir)
     }
-    else{
-        print(session_id)
-        print(as.character(session_id));
-        r <- GET(go_to_url,set_cookies("sails.sid=" = as.character(session_id)),add_headers(Cookie= paste0("sails.sid=",as.character(session_id))))
-        print(content(r)$cookies)
-        writeBin(content(r,'raw'),htmlFile)
-    }
+    tryCatch({
+        if(post){
+            r <- POST(go_to_url,config=(content_type_json()),body =body,encode="json")
+            writeBin(content(r,'raw'),htmlFile)
+        }
+        else{
+            r <- GET(go_to_url)
+            writeBin(content(r,'raw'),htmlFile)
+        }
+    },
+    error=function(cond){
+        print(cond);
+        print("Could not reach Rdocumentation, either your internet connection is bad or Rdocumentation is offline")
+        return (invisible())
+    })
     p <- tools::startDynamicHelp(NA)
     browser <-  getOption("browser")
     browseURL(paste0("http://127.0.0.1:", p, "/library/Rdocumentation/doc/index.html?viewer_pane=1&Rstudio_port=",
-        as.character(Sys.getenv("RSTUDIO_SESSION_PORT")),"&RS_SHARED_SECRET=",as.character(Sys.getenv("RS_SHARED_SECRET")),
-        "&S_id=",as.character(session_id),"&URL=",go_to_url),browser)
+        as.character(Sys.getenv("RSTUDIO_SESSION_PORT")),"&RS_SHARED_SECRET=",as.character(Sys.getenv("RS_SHARED_SECRET"))),browser)
     return (invisible())
 }
+#' @export
+check_package <- function(mypkg){
+    if(!is.element(mypkg, installed.packages()[,1])){
+        print("The package for this topic is not installed, try running:")
+        print(paste0("install.packages('",mypkg,"')"))
+        print("Or check the installation notes for the package with:")
+        print(paste0("help(package=",mypkg,")"))
+        return (FALSE)
+    }
+    else{
+        return (TRUE)
+    }
+} 
+
 library(proto)
-#TODO:check if browse_url in subfunction also fails
 #' @export
 help <- with(proto(environment(help), help = utils::help, browseURL = .browseUrl.help,`class<-` = `.class.help<-`),help)
 this.help <- with(proto(environment(help), help = utils::help, browseURL = .browseUrl.help,`class<-` = `.class.help<-`),help)
@@ -93,4 +123,4 @@ this.help <- with(proto(environment(help), help = utils::help, browseURL = .brow
 help.search <- with(proto(environment(help), help.search = utils::help.search, `class<-` = `.class.help<-`),help.search)
 this.help.search <- with(proto(environment(help), help.search = utils::help.search, `class<-` = `.class.help<-`),help.search)
 #' @export
-`?` <- with(proto(environment(help), `?` = utils::`?`,help=this.help,help.search=this.help.search),`?`) 
+`?` <- with(proto(environment(help), `?` = utils::`?`,help=this.help,help.search=this.help.search),`?`)         
