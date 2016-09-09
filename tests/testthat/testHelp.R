46ac930b899f23792025d463f24e6fb60f9c6d89
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-09-09 14:30:01 +0200

    updated tests for new version

--- tests/testthat/testHelp.R
+++ tests/testthat/testHelp.R
@@ -2,14 +2,16 @@ library(Rdocumentation)
 context("help")
 
 test_that("help_normal",{
-	expect_equal(help(mean,base),NULL)
-	expect_equal(help(package=utils),NULL)
+	expect_equal(help(mean,base),invisible())
+	expect_equal(help(package=utils),invisible())
 })
 
 test_that("help_non_installed_package",{
-	expect_equal(help(package="non_installed"),NULL)
+	#Rdocumentation will not find this package
+	expect_error(help(package="non_installed"))
 })
 
 test_that("help_topic_non_installed_package",{
-	expect_equal(help("no_topic","non_installed"),NULL)
+	#Rdocumenation will not find the package, but can still give similar topics, contrary to the local function that would just throw an error
+	expect_equal(help("no_topic","non_installed"),invisible())
 })
--- tests/testthat/testHelpSearch.R
+++ tests/testthat/testHelpSearch.R
@@ -2,12 +2,12 @@ library(Rdocumentation)
 context("help.search")
 
 test_that("help_search",{
-	expect_equal(help.search("whatever"),NULL)
-	expect_equal(help.search("help"),NULL)
+	expect_equal(help.search("whatever"),invisible())
+	expect_equal(help.search("help"),invisible())
 });
 
 test_that("test_with_fields",{
-	expect_equal(help.search("help",fields=c("title")),NULL)
-	expect_equal(help.search("this",fields=c("title","alias")),NULL)
-	expect_equal(help.search("testing",fields=c("title","alias","concept")),NULL)
+	expect_equal(help.search("help",fields=c("title")),invisible())
+	expect_equal(help.search("this",fields=c("title","alias")),invisible())
+	expect_equal(help.search("testing",fields=c("title","alias","concept")),invisible())
 })
