42428f81738a33eda4fecc9c8612a429be18be2f
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-10-19 18:18:29 +0200

    staging url

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-rdocs_url <- "http://www.rdocumentation.org/"
+rdocs_url <- "http://staging.rdocumentation.org/"
 rdocs_dir <- file.path(system.file(package = "RDocumentation"), "doc")
 html_file <- file.path(rdocs_dir, "index.html")
 cred_path <- file.path(system.file(package = "RDocumentation"), "config", "creds.txt")
